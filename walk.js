2cf3818a487a47f34fa51d8ee3a54c751d2ad255
Slava <slava91petrushin@yandex.ru>
2019-10-01 21:15:07 +0300

    for sozvona

--- walk.js
+++ walk.js
@@ -1,39 +1,33 @@
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
 const readdir = util.promisify(fs.readdir);
 
 export default function walk (dir, callbackOnFile, callbackOnFolder, done) {
-  fs.readdir(dir, (err, list) => {
-    if (err) return done(err);
-    let i = 0;
-    const next = function(doneList) {
-      if (err) return doneList(err);
-			
-			let filePath = list[i++];
-
-      if (!filePath) return doneList(null);
-
-      filePath = path.join(dir, filePath);
-
-      fs.stat(filePath, (_, stat) => {
-
-        if (stat && stat.isDirectory()) {
-          walk(
-            filePath,
-            callbackOnFile,
-            callbackOnFolder,
-            next.bind(null, doneList)
-          );
-        } else {
-          callbackOnFile(filePath, next.bind(null, doneList));
-        }
-      });
-    };
-
-    next(err => {
-      if (!err) callbackOnFolder(dir);
-      done(err);
-    });
-  });
+	readdir(dir)
+		.then(directory => {
+			return Promise.all(directory.map(async item => {
+				let filePath = path.join(dir, item);
+				fs.stat(filePath, (err, stats)=>{
+					if (stats && stats.isDirectory()) {
+						walk(
+							filePath,
+							callbackOnFile,
+							callbackOnFolder,
+							err => {
+								console.log("Error: ", err);
+							}
+						);
+					} else {
+						callbackOnFile(filePath,  err => {
+							console.log("Error: ", err);
+						});
+					}						
+				})
+			}))
+		})
+		.then(() => callbackOnFolder(dir))
+		.catch(err =>  done(err))
 };
+
 
